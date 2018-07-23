d6a3cdbb9ed7a23fd703f711fbb3eb5a9be63312
Ondrej Kipila <ondrej.kipila@pixwell.sk>
2018-07-23 16:31:47 +0100

    Add View.toBitmap() extension

--- core/src/main/java/sk/pixwell/android/core/ui/widget/View.kt
+++ core/src/main/java/sk/pixwell/android/core/ui/widget/View.kt
@@ -1,6 +1,10 @@
 package sk.pixwell.android.core.ui.widget
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 
@@ -15,4 +19,18 @@ fun View.hideKeyboard() {
     val inputMethodManager =
         context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.hideSoftInputFromWindow(windowToken, 0)
+}
+
+fun View.toBitmap(): Bitmap {
+    measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED)
+    layout(0, 0, measuredWidth, measuredHeight)
+    val bitmap = Bitmap.createBitmap(
+        measuredWidth, measuredHeight,
+        Bitmap.Config.ARGB_8888
+    )
+    val canvas = Canvas(bitmap)
+    canvas.drawColor(Color.WHITE, PorterDuff.Mode.SRC_IN)
+    background?.draw(canvas)
+    draw(canvas)
+    return bitmap
 }
